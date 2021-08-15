b30f0260642d974ec580457c81a0a8eb3b70f678
blocktaco <60831236+blocktaco@users.noreply.github.com>
2021-08-15 17:44:54 -0500

    Update Input.lua

--- UI/Util/Input.lua
+++ UI/Util/Input.lua
@@ -1,1 +1,11 @@
+local userInputService = game:GetService('UserInputService')
+local Input = {}
 
+function Input.isMouseBounds(drawing)
+    local pX, pY, sX, sY = drawing.Position.X, drawing.Position.Y, drawing.Size.X, drawing.Size.Y
+	local mouse = userInputService:GetMouseLocation()
+	
+	return mouse.X >= pX and mouse.X <= pX + sX and mouse.Y >= pY and mouse.Y <= pY + sY or false
+end
+
+return Input;
